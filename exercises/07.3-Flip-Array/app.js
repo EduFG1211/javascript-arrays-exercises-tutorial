2840bb8a2f1dca87d576414655d5e364920fda2b
Edventures <edufg13@gmail.com>
2021-02-24 07:40:11 +0000

    voy en p3.1

--- exercises/07.3-Flip-Array/app.js
+++ exercises/07.3-Flip-Array/app.js
@@ -1,3 +1,10 @@
 var arr = [45,67,87,23,5,32,60];
 
 //Your code here.
+let arrInv = [];
+let arrlength = arr.length - 1;
+for (let i = arrlength; i >= 0; i--) {
+    arrInv[arrlength-i] = arr[i];    
+}
+//console.log(arr.reverse());
+console.log(arrInv);
